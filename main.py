496dde456d5f745701145b526a257b462ff3eb6b
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-03-11 16:44:39 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-from RoiClass import ComposeROI
-from video_streamer import VideoPlayer
+# main.py
 
-comp_roi = ComposeROI("params2.json")
-video_player = VideoPlayer("./data/crusher_closeup.mp4", comp_roi)
-video_player.run()
+from RoiClass import ComposeROI
+from other import process_roi
+from video_streamer2 import VideoStream
+import cv2
+
+
+def main():
+    video_path = "./data/crusher_closeup.mp4"
+
+    compose_roi = ComposeROI("params2.json")
+    roi_points = [roi.points for roi in compose_roi]
+
+    vs = VideoStream(cam_name="Video Stream", path=video_path, roi_points=roi_points, transform=process_roi)
+    vs.start()
+
+    while True:
+        if not vs.running():
+            break
+
+        vs.show_frame()
+
+    vs.stop()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
