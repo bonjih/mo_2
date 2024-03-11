ff48540f8a885a460e5fb1db90353904451312ed
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-03-11 11:11:50 +1000

    Create RoiClass.py

---
+++ RoiClass.py
@@ -0,0 +1,23 @@
+import json
+
+
+class ROI:
+    def __init__(self, **kwargs):
+        self.points = kwargs
+
+
+class ComposeROI:
+    def __init__(self, json_file):
+        self.rois = []
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+            for roi_name, roi_data in data.items():
+                if roi_name.startswith("roi"):
+                    roi = ROI(**roi_data)
+                    self.rois.append(roi)
+
+    def add_roi(self, roi):
+        self.rois.append(roi)
+
+    def __iter__(self):
+        return iter(self.rois)
