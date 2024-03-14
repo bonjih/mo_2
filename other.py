8b7d06aebdb5fe246f3f3549bdcee820b03b5164
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-03-14 14:08:53 +0800

    Update other.py

--- other.py
+++ other.py
@@ -1,34 +1,67 @@
-import cv2
-import numpy as np
-
-
-def frame_diff(first_roi, second_roi):
-    diff_im = cv2.subtract(second_roi, first_roi)
-    conv_gray = cv2.cvtColor(diff_im, cv2.COLOR_RGB2GRAY)
-    ret, mask = cv2.threshold(conv_gray, 100, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-
-    # Ensure proper datatype and dimensions of mask
-    mask = mask.astype(np.uint8)
-
-    # Create a copy of the difference image
-    diff_im_colored = cv2.cvtColor(diff_im, cv2.COLOR_RGB2BGR)
-
-    # Set pixels where the mask is true to red in the colored difference image
-    diff_im_colored[mask != 255] = [0, 0, 255]
-
-    return diff_im_colored
-
-def frame_preprocess(frame_ref):
-    return cv2.cvtColor(frame_ref, cv2.COLOR_BGR2RGB)
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
-def process_roi(frame, prev_frame, roi_points):
-    for points in roi_points:
-        x1, y1 = points['x2'], points['y2']
-        x4, y4 = points['x3'], points['y4']
-        prev_roi_region = prev_frame[y1:y4, x1:x4]
-        frame_roi_region = frame[y1:y4, x1:x4]
-        diff_im = frame_diff(frame_preprocess(prev_roi_region), frame_preprocess(frame_roi_region))
-        frame[y1:y4, x1:x4] = diff_im
-    return frame
+def get_df_vals(seconds: float, fps: int, df_lst: list, sum, sma_params: dict):
+    """
+        fps: input video frame rate as an int
+        seconds: input video timestamp
+        df_list: reference output list for sma calculation
+        params: parameters for sma and thresholds settings
+
+    """
+    sma = 0.0
+    fps_n = sma_params["interval_seconds"] * fps - 1
+
+    if len(df_lst) >= fps_n:
+        for i in range(len(df_lst) - 1, (len(df_lst) - 1) - fps_n, -1):
+            sma = sma + df_lst[i]["motion_sum"]
+
+    sma = (sma + sum) / float(fps_n)
+
+    df_vals = {
+        "sec": seconds,
+        "motion_sum": sum,
+        "motion_sma": sma,
+        "motion_exceeded_35": sma >= sma_params["t0"],
+        # 0.2[0.0, 152974.7, 305949.4, 458924.1, 611898.8, 764873.5, 917848.2, 1070822.9000000001, 1223797.6, 1376772.3, 1529747.0]
+        "motion_exceeded_50": sma >= sma_params["t1"],
+        # 0.25  # [1529747.0, 1682721.7, 1835696.4, 1988671.1, 2141645.8000000003, 2294620.5, 2447595.2, 2600569.9000000004, 2753544.6, 2906519.3, 3059494.0]
+    }
+    return df_vals
+
+
+def calc_area_percent(tl, tr, bl, C):
+    """
+    calculaes the percentage/area of the bounding box
+    for the purpose of excluding small areas
+    :param tl: bb coords top left
+    :param tr: bb coords top right
+    :param bl: bb coords bottom left
+    :param C: bb coords bottom right
+    :return: BB area percentage contours fill it
+    """
+    box_area = (tr[0] - tl[0]) * (bl[1] - tl[1])
+    topmost = tuple(C[C[:, :, 1].argmin()][0])
+    percent = 1 - ((topmost[1] - 340) / 740)
+    return box_area, percent
+
+
+def write_csv(intensity):
+    data = {'intensity_Sum': [intensity]}
+    df = pd.DataFrame(data)
+
+    with open('intensity.csv', 'a') as csvfile:
+        df.to_csv(csvfile, header=None, mode='a', index=False)
+
+
+def plot_histo(csv_file):
+    df = pd.read_csv(csv_file)
+    plt.hist(df['intensity_Sum'], bins=5, color='blue')
+    plt.grid(True)
+    plt.show()
+
+
+#plot_histo('intensity.csv')
+
